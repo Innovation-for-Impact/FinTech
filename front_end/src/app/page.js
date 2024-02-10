f127c1324472c06cc5fe332f0a4e38c077e51bd3
Julia Tauro <jtauro@Julias-MacBook-Pro.local>
2024-02-10 15:18:32 -0500

    made many changes

--- front_end/src/app/page.js
+++ front_end/src/app/page.js
@@ -1,95 +1,48 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../app/css/page.module.css";
+import logo from '../app/images/icon_transparent.png';
+
+console.log(logo); // /logo.84287d09.png
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+    <div className={styles.login}>
+      <img
+        className={styles.img}
+        src="/_next/static/media/icon_transparent.e1a2640c.png"
+        alt="Grapefruit slice atop a pile of other slices" 
+      />
+      <h2>WELCOME BACK!</h2>
+        <div className={styles.info}>
+          <label>
+            <input 
+              className={styles.input}
+              type="text" 
+              name="username" 
+              placeholder="Username"></input>
+          </label>
+          <input 
+            className={styles.input}
+            type="password" 
+            name="password" 
+            placeholder="Password"></input>
+          <p className={styles.forgot}>
+            <a href = "recover.html">
+              Forgot Password?
+            </a>
+          </p> 
+          <button> 
+            LOGIN 
+          </button>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              Don't have an account?{" "} 
+              <a className={styles.a} href="signup.html">
+              Sign Up
+              </a>
+            </p>
+        </div>
+    </div>
     </main>
   );
 }
---
+++ front_end/src/app/recover.js
@@ -0,0 +1,40 @@
+import styles from "../app/css/page.module.css";
+import specificStyles from "../app/css/recover.module.css";
+
+console.log("this is the forgot password page");
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+      start working here!
+
+    </main>
+  );
+}
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <link rel="icon" type="image/x-icon" href="/images/icon.ico">
+//   <link rel="stylesheet" href="css/html5reset.css">
+//   <link rel="stylesheet" href="css/login.css">
+//  <title>FinTech</title>	
+// </head>	
+// <body>
+//    <main>
+//     <div class="login">
+//       <p>I'm sorry that you forgot your password :(</p>
+//       <p>Return to <a href = "index.html">Log In</a> page</p>
+//       </div>
+//    </main>  
+
+//    <footer>	
+//     <p>&copy; Innovation for Impact 2024</p>	
+//   </footer>	
+  
+// </body>
+// </html>
