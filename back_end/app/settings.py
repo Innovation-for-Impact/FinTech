bdef54f704097252c588f754705f52564c743961
Re <112631682+mcregan126@users.noreply.github.com>
2024-02-17 11:18:54 -0500

    Update settings.py

--- back_end/app/settings.py
+++ back_end/app/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'http://0.0.0.0',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
