0b95e34f963298551d07f306526e3ac5d34e7884
aasmart <alexandersmart2005@gmail.com>
2024-03-16 10:32:07 -0400

    Add route rewrite with testing server

--- front_end/next.config.mjs
+++ front_end/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+export async function rewrites() {
+    return [
+        {
+            source: "/api/:path*",
+            destination: `http://127.0.0.1:8000/api/:path*`,
+        }
+    ];
+}
