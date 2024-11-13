8f5015d817055de773bab09bcded1f1046ae37e4
aasmart <alexandersmart2005@gmail.com>
2024-11-12 20:07:53 -0500

    Add friends and groups models
    Built out friendship model and add proper manager controls for that. Add
    group model + frienship model, but without core logic just yet.
    
    Also introduces unit tests for friendships

--- back_end/innofunds/models.py
+++ back_end/innofunds/models.py
@@ -1,14 +1,24 @@
+from enum import unique
 from django.db import models
 
-from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
+from itertools import chain
+
+from django.utils.module_loading import module_dir
+
 
 class FintechGroup(models.Model):
     id = models.BigAutoField(unique=True, primary_key=True)
-    name = models.CharField(max_length = 255)
-    description = models.CharField(max_length = 1000)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
 
-# USER
+
+class FintechGroupMembership(models.Model):
+    id = models.BigAutoField(unique=True, primary_key=True)
+    group_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+
 class FintechUserManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password=None):
         """
@@ -23,7 +33,7 @@ class FintechUserManager(BaseUserManager):
         user = self.model(
             email=self.normalize_email(email),
             first_name=first_name,
-            last_name = last_name
+            last_name=last_name,
         )
 
         user.set_password(password)
@@ -38,30 +48,29 @@ class FintechUserManager(BaseUserManager):
         user = self.create_user(
             email,
             first_name=first_name,
-            last_name = last_name,
+            last_name=last_name,
             password=password,
         )
         user.is_admin = True
         user.save(using=self._db)
         return user
 
+
 class FintechUser(AbstractUser):
     # ADD ANY FIELDS YOU NEED HERE
-    # !!! you will need to update the database after you make any changes here !!! 
+    # !!! you will need to update the database after you make any changes here !!!
     email = models.EmailField(
         verbose_name="email address",
         max_length=255,
         unique=True,
     )
+    username = models.CharField("username", max_length=255, unique=False)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
-    # users can be in many groups, and groups can have many users
-    groups = models.ManyToManyField(FintechGroup)
-
-    REQUIRED_FIELDS=["first_name", "last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name"]
     USERNAME_FIELD = "email"
     objects = FintechUserManager()
 
@@ -72,3 +81,63 @@ class FintechUser(AbstractUser):
     def is_staff(self):
         "Is the user a member of staff?"
         return self.is_admin
+
+
+class FintechFriendManager(models.Manager):
+    def create_friendship(self, user1, user2):
+        """Creates the friendship with the invariant that the first user
+        id id less than the second user's id"""
+        small = min(user1.id, user2.id)
+        big = max(user1.id, user2.id)
+
+        existing_friendship = (
+            self.get_queryset().filter(user1_pk=small).filter(user2_pk=big)
+        )
+        if existing_friendship.exists():
+            return existing_friendship
+
+        friendship = self.model(user1_pk=small, user2_pk=big)
+
+        friendship.save(using=self._db)
+        return friendship
+
+    def get_user_friendships(self, user):
+        """Returns a list of user IDs for the user's friends"""
+        friends_lower = self.get_queryset().filter(user1_pk=user.pk).values("user2_pk")
+        friends_upper = self.get_queryset().filter(user2_pk=user.pk).values("user1_pk")
+
+        # .values(...) returns a dict, so get rid of that
+        friends_lower = map(lambda user: user["user2_pk"], friends_lower)
+        friends_upper = map(lambda user: user["user1_pk"], friends_upper)
+
+        return list(chain(friends_lower, friends_upper))
+
+    def remove_friendship(self, user1, user2):
+        """Unfriends the given two users"""
+        small = min(user1.id, user2.id)
+        big = max(user1.id, user2.id)
+
+        friendship = self.get_queryset().filter(user1_pk=small).filter(user2_pk=big)
+        if not friendship.exists():
+            return
+
+        friendship.delete()
+
+        return
+
+    def have_friendship(self, user1, user2):
+        """Returns a boolean value indicating if the two users have
+        a friendship. True if a friendship does exist"""
+        small = min(user1.id, user2.id)
+        big = max(user1.id, user2.id)
+
+        friendship = self.get_queryset().filter(user1_pk=small).filter(user2_pk=big)
+        return friendship.exists()
+
+
+class FintechFriend(models.Model):
+    id = models.BigAutoField(unique=True, primary_key=True)
+    user1_pk = models.IntegerField()
+    user2_pk = models.IntegerField()
+
+    objects = FintechFriendManager()
--- back_end/innofunds/tests.py
+++ back_end/innofunds/tests.py
@@ -1,3 +1,109 @@
 from django.test import TestCase
 
-# Create your tests here.
+from innofunds.models import FintechFriend, FintechUser
+
+
+class FriendshipTestCases(TestCase):
+    def setUp(self):
+        FintechUser.objects.create(
+            password="abc",
+            email="example1@email.com",
+            first_name="Example",
+            last_name="Example",
+        )
+        FintechUser.objects.create(
+            password="abc",
+            email="example2@email.com",
+            first_name="Example",
+            last_name="Example",
+        )
+        FintechUser.objects.create(
+            password="abc",
+            email="example3@email.com",
+            first_name="Example",
+            last_name="Example",
+        )
+        FintechUser.objects.create(
+            password="abc",
+            email="example4@email.com",
+            first_name="Example",
+            last_name="Example",
+        )
+
+    def test_friendship_create_delete(self):
+        # user IDs begin at 1
+        user1 = FintechUser.objects.get(email="example1@email.com")
+        user2 = FintechUser.objects.get(email="example2@email.com")
+        FintechFriend.objects.create_friendship(user1, user2)
+
+        # should be friends with user 2
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        self.assertEqual(len(user1_friends), 1)
+        self.assertEqual(user1_friends[0], 2)
+
+        user2_friends = FintechFriend.objects.get_user_friendships(user2)
+        self.assertEqual(len(user2_friends), 1)
+        self.assertEqual(user2_friends[0], 1)
+
+        user3 = FintechUser.objects.get(email="example3@email.com")
+        FintechFriend.objects.create_friendship(user1, user3)
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        self.assertEqual(len(user1_friends), 2)
+        self.assertEqual(user1_friends, [2, 3])
+
+        FintechFriend.objects.remove_friendship(user1, user2)
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        user2_friends = FintechFriend.objects.get_user_friendships(user2)
+        self.assertEqual(len(user1_friends), 1)
+        self.assertEqual(user1_friends, [3])
+
+        self.assertEqual(len(user2_friends), 0)
+
+    def test_friendships_invariant(self):
+        user1 = FintechUser.objects.get(email="example1@email.com")
+        user2 = FintechUser.objects.get(email="example2@email.com")
+        user3 = FintechUser.objects.get(email="example3@email.com")
+
+        FintechFriend.objects.create_friendship(user1, user2)
+        FintechFriend.objects.create_friendship(user2, user3)
+
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        self.assertEqual(len(user1_friends), 1)
+        self.assertEqual(user1_friends[0], 2)
+        self.assertTrue(FintechFriend.objects.have_friendship(user1, user2))
+        self.assertFalse(FintechFriend.objects.have_friendship(user1, user3))
+
+        user2_friends = FintechFriend.objects.get_user_friendships(user2)
+        self.assertEqual(len(user2_friends), 2)
+        self.assertIn(1, user2_friends)
+        self.assertIn(3, user2_friends)
+        self.assertTrue(FintechFriend.objects.have_friendship(user2, user1))
+        self.assertTrue(FintechFriend.objects.have_friendship(user3, user2))
+
+        user3_friends = FintechFriend.objects.get_user_friendships(user3)
+        self.assertEqual(len(user3_friends), 1)
+        self.assertEqual(user1_friends[0], 2)
+        self.assertFalse(FintechFriend.objects.have_friendship(user3, user1))
+        self.assertTrue(FintechFriend.objects.have_friendship(user2, user3))
+
+        FintechFriend.objects.remove_friendship(user3, user2)
+
+        user2_friends = FintechFriend.objects.get_user_friendships(user2)
+        self.assertEqual(len(user2_friends), 1)
+        self.assertIn(1, user2_friends)
+
+        user3_friends = FintechFriend.objects.get_user_friendships(user3)
+        self.assertEqual(len(user3_friends), 0)
+
+    def test_no_friendship(self):
+        user1 = FintechUser.objects.get(email="example1@email.com")
+        user2 = FintechUser.objects.get(email="example2@email.com")
+
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        self.assertEqual(len(user1_friends), 0)
+
+        FintechFriend.objects.remove_friendship(user1, user2)
+        user1_friends = FintechFriend.objects.get_user_friendships(user1)
+        self.assertEqual(len(user1_friends), 0)
+        user2_friends = FintechFriend.objects.get_user_friendships(user2)
+        self.assertEqual(len(user2_friends), 0)
