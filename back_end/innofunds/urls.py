b784c172543ecdf30cba4555bf8a70753713e222
mcregan <mcregan@umich.edu>
2024-11-14 19:41:13 -0500

    api

--- back_end/innofunds/urls.py
+++ back_end/innofunds/urls.py
@@ -3,10 +3,15 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 import innofunds.views.users as user_views
 from rest_framework_nested import routers
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', viewset=user_views.FintechUserViewSet, basename='users')
 
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)), 
+    path('create_link_token/', views.create_link_token, name="create_link_token"),
+    path('exchange_public_token/', views.exchange_public_token, name="exchange_public_token"),
+    path('get_transactions/', views.get_transactions, name="get_transactions"),
 ]
--- back_end/plaid/auth.py
+++ back_end/plaid/auth.py
@@ -1,61 +1,108 @@
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 import http.client
 import json
+from datetime import datetime, timedelta
 
-file = open("keys.txt")
-keys = file.read().split()
+def create_link_token(request):
+    
+  client_id = "65e7b7407aa8cf001cc59e7b6247f4912edd996835254c9e47bbf4"
+  secret = "6247f4912edd996835254c9e47bbf4"
 
-client_id = keys[0]
-secret = keys[1]
+  conn = http.client.HTTPSConnection("sandbox.plaid.com")
+  headers = {'Content-Type': 'application/json'}
 
-file.close()
+  payload = json.dumps({
+      "client_id": client_id,
+      "secret": secret,
+      "client_name": "InnoBank user",
+      "country_codes": [
+      "US"
+      ],
+      "language": "en",
+      "user": {
+      "client_user_id": "unique_user_id"
+      },
+      "products": [
+      "auth"
+      ]
+    })
 
-conn = http.client.HTTPSConnection("sandbox.plaid.com")
-headers = {
-  'Content-Type': 'application/json'
-}
+  conn.request("POST", "/link/token/create", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
 
-# Create token
-payload = json.dumps({
-  "client_id": client_id,
-  "secret": secret,
-  "institution_id": "ins_20",
-  "initial_products": [
-    "auth"
-  ],
-  "options": {
-    "webhook": "https://www.genericwebhookurl.com/webhook"
-  }
-})
-conn.request("POST", "/sandbox/public_token/create", payload, headers)
-res = conn.getresponse()
-data = res.read()
-
-data_json = json.loads(data)
-publicToken = data_json["public_token"]
-
-# Exchange token for key
-payload = json.dumps({
-  "client_id": client_id,
-  "secret": secret,
-  "public_token": publicToken
-})
-
-conn.request("POST", "/item/public_token/exchange", payload, headers)
-res = conn.getresponse()
-data = res.read()
-
-data_json = json.loads(data)
-accessToken = data_json["access_token"]
+  link_token_response = json.loads(data.decode("utf-8"))
+  link_token = link_token_response['link_token']
+  return JsonResponse({'link_token': link_token})
 
 
-payload = json.dumps({
-  "client_id": client_id,
-  "secret": secret,
-  "access_token": accessToken
-})
+def exchange_public_token(request):
+    req_data = json.loads(request.body)
+    public_token = req_data.get('public_token')
 
-conn.request("POST", "/auth/get", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+    if not public_token:
+       return JsonResponse({'error': 'public_token is missing'}, status=400)
+
+
+    conn = http.client.HTTPSConnection("sandbox.plaid.com")
+    headers = {'Content-Type': 'application/json'}
+
+    payload = json.dumps({
+        "client_id": "65e7b7407aa8cf001cc59e7b6247f4912edd996835254c9e47bbf4",
+        "secret": "6247f4912edd996835254c9e47bbf4",
+        "public_token": public_token
+    })
+
+    conn.request("POST", "/item/public_token/exchange", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    exchange_response = json.loads(data.decode("utf-8"))
+    access_token = exchange_response.get('access_token')
+
+    if not access_token:
+        return JsonResponse({'error': 'Failed to exchange public_token'}, status=401)
+
+    request.session['access_token'] = access_token
+
+    return JsonResponse({'access_token': access_token})
+
+
+def get_transactions(request):
+  access_token = request.session.get('access_token')
+  if not access_token:
+      return JsonResponse({'error': 'Access token is missing'}, status=401)
+
+
+  start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+  end_date = datetime.now().strftime('%Y-%m-%d')
+
+    #connect to Plaid API
+  conn = http.client.HTTPSConnection("sandbox.plaid.com")
+  headers = {'Content-Type': 'application/json'}
+
+  payload = json.dumps({
+     "client_id": "65e7b7407aa8cf001cc59e7b6247f4912edd996835254c9e47bbf4",
+      "secret": "6247f4912edd996835254c9e47bbf4",
+      "access_token": access_token,
+      "start_date": start_date,
+      "end_date": end_date,
+      "options": {
+          "count": 10,
+          "offset": 0
+      }
+  })
+
+    #send request to Plaid
+  conn.request("POST", "/transactions/get", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+
+    #parse response
+  transactions_response = json.loads(data.decode("utf-8"))
+  transactions = transactions_response.get('transactions', [])
+
+  return JsonResponse({'transactions': transactions})
 
