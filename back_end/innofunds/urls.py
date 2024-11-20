d283237345eb076bfb3c201e1f893a4efb38036f
ethansun <ethansun@umich.edu>
2024-11-20 14:18:03 -0500

    code compiles now

--- back_end/innofunds/urls.py
+++ back_end/innofunds/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 import innofunds.views.users as user_views
 from rest_framework_nested import routers
-from . import views
+from .views import plaid
 
 router = routers.DefaultRouter()
 router.register(r'users', viewset=user_views.FintechUserViewSet, basename='users')
@@ -11,7 +11,7 @@ router.register(r'users', viewset=user_views.FintechUserViewSet, basename='users
 
 urlpatterns = [
     path('', include(router.urls)), 
-    path('create_link_token/', views.create_link_token, name="create_link_token"),
-    path('exchange_public_token/', views.exchange_public_token, name="exchange_public_token"),
-    path('get_transactions/', views.get_transactions, name="get_transactions"),
+    path('create_link_token/', plaid.create_link_token, name="create_link_token"),
+    path('exchange_public_token/', plaid.exchange_public_token, name="exchange_public_token"),
+    path('get_transactions/', plaid.get_transactions, name="get_transactions"),
 ]
